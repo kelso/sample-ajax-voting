7783ae365b2f814ccc053121dc9268e2480c6fe1
Stefan Huska <stefan.huska@gmail.com>
2014-05-15 19:16:53 +0200

    Initial commit, added Post model & controller.

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+# sample posts
+Post.create(title: 'First Post', body: 'Velit et aute ut et quis ullamco dolore sed ex labore laboris reprehenderit commodo aute reprehenderit nisi aute proident labore ut dolore in tempor quis cupidatat.')
+Post.create(title: 'Second Post', body: 'Exercitation dolor ex adipisicing sed elit in commodo est ut id dolore ex nostrud ad velit velit enim deserunt officia reprehenderit nostrud id dolore sed cillum voluptate voluptate.')
